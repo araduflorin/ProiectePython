2ad4d12a2c3ff00baa7dfad5cecea3b08191ce00
araduflorin <araduflorin@yahoo.com>
2023-06-16 15:09:49 +0300

    Commit3

--- Top_filme.py
+++ Top_filme.py
@@ -1,40 +1,36 @@
+# d1 = {'nume': 'George', 'filme': ['Shrek', 'Bond', 'Fight Club'] }
+# d2 = { 'nume' : 'Cristian', 'filme': ['Fight Club', 'The Nun', 'Dracula', 'Bond']}
+# d3 = { 'nume' : 'Stefan', 'filme': ['Fight Club', 'Slumdog Milionare'] }
+# v = [ d1, d2, d3 ]
 
-# while True:
-#       nr = int(input("Intoduceti un nr intreg: "))
-#       if nr % 3 == 0:
-#          print("Fizz")
-#          #break
-#       elif nr % 5 == 0:
-#          print("Buzz")
-#          #break
-#       elif (nr % 3 == 0) and (nr % 5 == 0):
-#          print("FizzBuzz")
-#          #break
-#       else:
-#          print(nr)
+lista_filme = [ { 'nume': 'George', 'filme': ['Shrek', 'Bond', 'Fight Club'] },
+{ 'nume' : 'Cristian', 'filme': ['Fight Club', 'The Nun', 'Dracula', 'Bond']},
+{ 'nume' : 'Stefan', 'filme': ['Fight Club', 'Slumdog Milionare']}]
 
+lst_film = lista_filme[0]["filme"] + lista_filme[1]["filme"] + lista_filme[2]["filme"]
+count = {}
+for f in lst_film:
+    if f in count:
+        count[f] += 1
+    else:
+        count[f] = 1
+print(count)
+for i,j in count.items():
+#    if j > 0:
+        print(f"{i} a fost vizionat de {j} ori")
+print(lst_film)
 
-# nr = int(input("Intoduceti un nr intreg: "))
-
-# while True:
-#     nr = int(input("Intoduceti un nr intreg: "))
-#     if (nr % 3 == 0) and (nr % 5 == 0):
-#         print("FizzBuzz")
-#     elif nr % 3 == 0:
-#         print("Fizz")
-#     elif nr % 5 == 0:
-#         print("Buzz")
+# my_list = ['a', 'b', 'c', 'a', 'd', 'e', 'b', 'a']
+# count_dict = {}
+#
+# # loop through the list and count occurrences of each element
+# for element in my_list:
+#     if element in count_dict:
+#         count_dict[element] += 1
 #     else:
-#         print(nr)
-
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-c = []
-for i in a:
-    if i in b:
-        c.append(i)
-print(list(set(c)))
-
-
-# print(set(a))
-# print(set(b))
+#         count_dict[element] = 1
+#
+# print(count_dict)
+# for element, count in count_dict.items():
+#     if count > 1:
+#         print(f"{element} appears {count} times in the list.")
