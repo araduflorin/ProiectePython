5ac69d5516d26a61d088b1c08ad6b3f7a993be16
araduflorin <araduflorin@yahoo.com>
2023-06-30 15:11:56 +0300

    Commit10

--- Validator_CNP.py
+++ Validator_CNP.py
@@ -1,56 +1,57 @@
 CNP = {'S': [], 'AA': 0, 'LL': 0, 'ZZ': 0, 'JJ': 0, 'NNN': 0, 'C': 0}
-# S = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+list_S = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+list_LL = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 AA = []
 
 
-# print(S)
-def validare_S(nr_S):
-    S = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    while (nr_S[0]) not in S:
-        nr_S = input("Prima cifra trebuie sa fie intre 1 si 9: ")
-        while int(nr_S[1:3]) > 23:
-            nr_S = input("Cifrele 2 si 3 tr sa fie < 24: ")
-    # print(nr)
-    # print(int(nr_S[1:3]))
-    return nr_S
-
-# def validare_AA(nr_An):
-#     # validare_input_numar(nr_An)
-#     # print("Cifrele 2 si 3: ",int(nr_An[1:3]))
-#     while int(nr_An[1:3]) > 23:
-#         nr_An = input("Cifrele 2 si 3 tr sa fie < 24: ")
-#     return nr_An
-
-
-def validare_input_numar(nr):
-    for i in range(len(nr)):
-        # print(list(nr[0]))
+def number_validate(nr):
+    # print((nr[3:5]))
+    while True:
         if str(nr).isdigit() is False:
+            # if str(nr).isdigit() is False:
             nr = input("Introdu doar cifre: ")
-        elif len(nr) != 13:
+            continue
+        if len(nr) != 13:
             nr = input("Introdu 13 cifre: ")
-
-    # for j in list(nr):
-    #     print(j)
-    #
-
-    # if list(nr) > str(23):
-    #     nr = input("Cifrele 2 si 3 tr sa fie < 24: ")
+            continue
+        if (nr[0]) not in list_S:
+            nr = input("Prima cifra trebuie sa fie intre 1 si 9: ")
+            continue
+        if (nr[0]) in ['5', '6'] and int(nr[1:3]) > 23:
+            nr = input("Cifrele 2 si 3 tr sa fie < 24: ")
+            continue
+        if (nr[3:5]) not in list_LL:
+            nr = input("Cifrele 4 si 5 (luna) tb. sa fie cuprinse in intevalul 01-12: ")
+            continue
+        break
     return nr
+
+
+# def length_validate(nr_length):
+#     while len(nr_length) != 13:
+#         nr_length = input("Introdu 13 cifre: ")
+#     return nr_length
 
 
 def input_CNP():
     # validare_S(nr)
     # validare_AA(nr)
-    return validare_input_numar(input("Introdu CNP-ul format din 13 cifre: "))
+    return number_validate(input("Introdu CNP-ul format din 13 cifre: "))
+
 
 def calcul():
     cnp = input_CNP()
-    validare_S(cnp)
+    number_validate(cnp)
+    # length_validate(cnp)
+    # validate_S(cnp)
+    # validate_AA(cnp)
+    # validare_S(cnp)
     # validare_AA(cnp)
     return cnp
+
+
 # c=input_CNP()
 # print(c)
-calcul()
+print(calcul())
 
 # Valideaza S
