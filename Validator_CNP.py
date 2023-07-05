09059b6a626867d86458c0c866f2516b0fcc9e06
araduflorin <araduflorin@yahoo.com>
2023-07-05 15:12:35 +0300

    Commit11

--- Validator_CNP.py
+++ Validator_CNP.py
@@ -1,4 +1,4 @@
-CNP = {'S': [], 'AA': 0, 'LL': 0, 'ZZ': 0, 'JJ': 0, 'NNN': 0, 'C': 0}
+
 list_S = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 list_LL = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 list_ZZ = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
@@ -8,70 +8,51 @@ list_JJ = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12
            '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
            '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36',
            '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '51', '52']
-# list_NNN = []
-# for i in range(1, 1000):
-#     list_NNN.append('{0:03}'.format(i))
-AA = []
 
+def nr_control_validation(cnp):
+    nr_control = '279146358279'
+    list_multiplication = [int(nr_control[i]) * int(cnp[i]) for i in range(0, len(list(nr_control)))]
+    sum_control = 0
+    for j in list_multiplication:
+        sum_control = sum_control + int(j)
+    return sum_control % 11
 
-def number_validate(nr):
-    # print((nr[3:5]))
+def number_validate():
+    nr = input("Introdu CNP-ul format din 13 cifre: ")
     while True:
-        if str(nr).isdigit() is False:
+        if nr.strip().isdigit() is False:
+            # print(type(nr))
+            # print(nr.isdigit())
             # if str(nr).isdigit() is False:
-            nr = input("Introdu doar cifre: ")
+            nr = input("Introduceti doar cifre: ")
             continue
         if len(nr) != 13:
-            nr = input("Introdu 13 cifre: ")
+            nr = input("Introduceti 13 cifre: ")
             continue
+
         if (nr[0]) not in list_S:
-            nr = input("Prima cifra trebuie sa fie intre 1 si 9: ")
+            nr = input(f"Prima cifra ({nr[0]}) trebuie sa fie intre 1 si 9: ")
             continue
         if (nr[0]) in ['5', '6'] and int(nr[1:3]) > 23:
-            nr = input("Cifrele 2 si 3 tr sa fie < 24: ")
+            nr = input(f"Nr. format din cifrele 2 si 3 ({nr[1:3]}) tr sa fie < 24: ")
             continue
         if (nr[3:5]) not in list_LL:
-            nr = input("Cifrele 4 si 5 (luna) tb. sa fie cuprinse in intevalul 01-12: ")
+            nr = input(f"Nr. format din cifrele 4 si 5 ({nr[3:5]}), luna, tb. sa fie cuprinse in intevalul 01-12: ")
             continue
         if (nr[5:7]) not in list_ZZ:
-            nr = input("Cifrele 6 si 7 (ziua) tb. sa fie cuprinse in intevalul 01-31: ")
+            nr = input(f"Nr. format din cifrele 6 si 7 ({nr[5:7]}), ziua, tb. sa fie cuprinse in intevalul 01-31: ")
             continue
         if (nr[7:9]) not in list_JJ:
-            nr = input("Cifrele 8 si 9 (judetul) tb. sa fie cuprinse in intervalele 01-46 si 51-52: ")
+            nr = input(f"Nr. format din cifrele 8 si 9 ({nr[7:9]}), judetul, tb. sa fie cuprinse in intervalele 01-46 si 51-52: ")
             continue
         if (nr[9:12]) == '000':
-            nr = input("Cifrele 10, 11 si 12 tb. sa fie un nr cuprins in intervalul 001-999: ")
+            nr = input(f"Nr. format din cifrele 10, 11 si 12 ({nr[9:12]}) tb. sa fie un nr cuprins in intervalul 001-999: ")
             continue
+        if int(nr[12]) != nr_control_validation(nr):
+            nr = input(f"Cifra 13 nu este valida. Trebuie sa fie {nr_control_validation(nr)} : ")
+            continue
+        print(f"CNP-ul {nr} este valid!")
         break
-    return nr
+    return
 
-
-def input_CNP():
-
-    return number_validate(input("Introdu CNP-ul format din 13 cifre: "))
-
-
-def calcul():
-    cnp = input_CNP()
-    number_validate(cnp)
-
-    return cnp
-
-
-# nr = '279146358279'
-# cnp = '178111529314'
-# list_nr = list(nr)
-# list_cnp = list(cnp)
-# c = []
-# for i in range(0, len(list_nr)):
-#     c.append(int(list_nr[i]) * int(list_cnp[i]))
-# s = 0
-# for j in c:
-#     s = s + int(j)
-#
-# print(c)
-# print(s)
-
-print(calcul())
-
-
+number_validate()
