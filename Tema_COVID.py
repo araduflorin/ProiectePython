be8163e1f3dd6fdf19009ae2e8d43a548ddbe469
araduflorin <araduflorin@yahoo.com>
2023-08-06 17:10:25 +0300

    Commit3

--- Tema_COVID.py
+++ Tema_COVID.py
@@ -9,7 +9,22 @@ import csv
 
 option = webdriver.ChromeOptions()
 option.add_argument("start-maximized")
-# <<<<<<< HEAD
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
+# driver.get("https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-1-martie-ora-13-00-2/")
+# driver.find_element(By.CLASS_NAME, 'swal2-confirm').click()
+# rows = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr"))
+# rows = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr"))
+# print(rows)
+# r = ''
+# for i in range(1, rows):
+#     row1 = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[" + str(rows) + "]").text
+#     r = row1
+# # print(r)
+#
+# # row1 = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[" + str(r) + "]").text
+# # row1 = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[last()]").text
+# print("Ultima linie:", r)
+
 # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
 # driver.get('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-1-martie-ora-13-00-2/')
 # rows = driver.fin("//*[@id='proxylisttable']/tbody/tr")
@@ -21,24 +36,36 @@ def get_link(link):
     driver.find_element(By.CLASS_NAME, 'swal2-confirm').click()
     rows = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr"))
     cols = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[1]/td"))
-    lst=[]
-    for r in range(2, 44):
+    cols_last_row = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr["+ str(rows) +"]/td"))
+    # print(cols_last_row)
+    # last_row = ''
+    # l = []
+    # # for row in range(1, rows+1):
+    # for p in range(1, cols):
+    #     for row in range(1, rows + 1):
+    #         last_row = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[" + str(rows) + "]/td[" + str(p) + "]").text
+    #         r=last_row
+    #     l.append(last_row)
+    # print(type(l))
+    # print(len(l))
+    # print(l[0:2])
+
+    lst = []
+    # row1 = ''
+    for r in range(2, rows+1):
         for p in range(1, cols - 1):
             value = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[" + str(r) + "]/td[" + str(p) + "]").text
+            # row1 = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[-1]").text
             lst.append(value)
+            # list_row.append(last_row)
+        # lst.append(row)
+    # print(lst)
+    # lst.append(l[0:2])
+    # del lst[-2:-4]
+    print("Lista: ", lst)
     return lst
 # time.sleep(2)
-# table = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table/tbody/tr['+'10'+']')
-# table = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[11]')
-# table1 = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[2]/td[1]')
 
-# lista = table.text.split('\n')
-# lista1 = table1.text.split('\n')
-# print(lista)
-# print(lista1)
-
-# rows = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr"))
-#
 # # Obtain the number of columns in table
 # cols = len(driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[1]/td"))
 # list_col = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[1]/td")
@@ -46,8 +73,8 @@ def get_link(link):
 link1 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-1-martie-ora-13-00-2/')
 link2 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-2-martie-ora-13-00-2/')
 link3 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-3-martie-ora-13-00-2/')
-link4 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-4-martie-ora-13-00/')
-link5 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-4-martie-ora-13-00-2/')
+link4 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-4-martie-ora-13-00-3/')
+link5 = get_link('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-5-martie-ora-13-00/')
 
 # print(link1)
 # print(link2)
@@ -78,10 +105,10 @@ dic = {}
 for i in range(0, len(link1), 3):
         # dic[link1[i], link1[i+1]] = [link1[i+2], link2[i+2], link3[i+2], link4[i+2], link5[i+2]]
         # dic.add[link1[i], link1[i+1], link1[i+2], link2[i+2], link3[i+2], link4[i+2], link5[i+2]]
-        dic[i] = [link1[i], link1[i+1], link1[i+2], link2[i+2], link3[i+2], link4[i+2], link5[i+2]]
+    dic[i] = [link1[i], link1[i+1], link1[i+2], link2[i+2], link3[i+2], link4[i+2], link5[i+2]]
 print(dic)
 list_header = ['Nr.Crt','Judet','01.03','02.03','03.03','04.03','05.03']
-pd.options.display.float_format = '{:,.2f}'.format
+# pd.options.display.float_format = '{:,.2f}'.format
 df = pd.DataFrame(dic).transpose()
 # df.head(list_header)
 # df.columns=list_header
@@ -90,42 +117,8 @@ df = pd.DataFrame(dic).transpose()
         # df = pd.DataFrame(lst)
 #
 
-df.to_csv('Covid_data.csv', sep='|', header=list_header, index=False)
 
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-# driver.get('https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-1-martie-ora-13-00-2/')
-# table = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody')
-# table1 = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/div[1]/main/article/div/div/table[1]/tbody/tr[4]')
-#
-# lista = table.text.split('\n')
-# lista1 = table.text.split('\n')
-# print(lista)
-# print(lista1)
+update_df = df.drop([df.index[-2], df.index[-3]])
+update_df.to_csv('Covid_data.csv', sep='|', header=list_header, index=False)
 
 
-# get_element = driver.find_element(by=By.ID, value='searchboxTrigger')
-# get_element.send_keys('telefon')
-# # time.sleep(10)
-# get_element.submit()
-# element = driver.find_elements(by=By.CLASS_NAME, value='card-item')
-# # print(element.text)
-# list_of_elements, product_list, price_list = [], [], []
-# for i in element:
-#     try:
-#         product = i.find_element(by=By.CLASS_NAME, value='card-v2-title-wrapper')
-#         product_list.append(product.text)
-#         price = i.find_element(by=By.CLASS_NAME, value='product-new-price')
-#         price_list.append(price.text)
-#         # print(price.text)
-#
-#     except exceptions.NoSuchElementException:
-#         pass
-# print(product_list)
-# list_of_elements.append(product_list)
-# list_of_elements.append(price_list)
-# print(list_of_elements)
-#
-# # with open('emag_products.csv', 'w') as file:
-# #     variabila = csv.writer(file, delimiter='|')
-# #     variabila.writerows(list_of_elements)
-# df = pd.DataFrame(list_of_elements).transpose()
